42c24e645f325efd5a921fdbde28e31088977eea
cyfinfaza <cyfinfaza@gmail.com>
2022-06-14 11:22:55 -0400

    fix donate button

--- src/routes/donate-and-volunteer.svelte
+++ src/routes/donate-and-volunteer.svelte
@@ -10,7 +10,7 @@
 	<h2>Volunteering</h2>
 	<p>If interested in volunteering, reach out to us!</p>
 	<p>
-		Email: <a href="mailto:ourdailybreadyork@gmail.com" >ourdailybreadyork@gmail.com</a>
+		Email: <a href="mailto:ourdailybreadyork@gmail.com">ourdailybreadyork@gmail.com</a>
 	</p>
 	<p>
 		Phone Number: <a href="tel:+1 717-848-1674">717-848-1674</a>
@@ -18,22 +18,29 @@
 	<p>Please provide us with your name, email address, phone number, and availability.</p>
 	<h2>Food Donations</h2>
 	<p>
-		Individual donations of desserts, produce, or nonperishable goods can be dropped off at <br>331 S
-		George St, York, PA 17401.
+		Individual donations of desserts, produce, or nonperishable goods can be dropped off at <br
+		/>331 S George St, York, PA 17401.
 	</p>
 	<p>If you have a casserole or other transportable goods, please give us a call (717-848-1674)!</p>
 	<h2>Monetary Donations</h2>
+	<p>We take online donations via facebook. Click below to donate!</p>
 	<p>
-		We take online donations via facebook. Click below to donate!</p>
-		<p>
-		<button style="height:34px; width:140px; font-size:16px;" onclick="location.href='https://m.facebook.com/sg/donate_check_out/?fundraiser_id=646512785771210&donate_ref=call_to_action&source_data%5Bsource_name%5D=page_header_cta&source_data%5Bsource_id%5D=646512785771210&source_data%5Bquick_promotion_id%5D&source_data%5Bmsite_source_interface%5D=iphone&is_monthly_subscription=0&deeplink_mode=basic&redirected_for_ios=1&_ft_=ftmd_400706.111111l&_rdr';" >Donate Online</button> 
+		<a
+			class="button donateButton"
+			href="https://m.facebook.com/sg/donate_check_out/?fundraiser_id=646512785771210&donate_ref=call_to_action&source_data%5Bsource_name%5D=page_header_cta&source_data%5Bsource_id%5D=646512785771210&source_data%5Bquick_promotion_id%5D&source_data%5Bmsite_source_interface%5D=iphone&is_monthly_subscription=0&deeplink_mode=basic&redirected_for_ios=1&_ft_=ftmd_400706.111111l&_rdr"
+			>Donate Online</a
+		>
 	</p>
 	<p>
-		Or if you prefer, you can <strong>send us a check</strong> made out to Our Daily Bread York. 
+		Or if you prefer, you can <strong>send us a check</strong> made out to Our Daily Bread York.
 	</p>
-	<p>Just <strong>
-		$50 can provide 25 meals!
-	</strong></p>
+	<p>Just <strong> $50 can provide 25 meals! </strong></p>
 	<p>We thank you for your support and generosity!</p>
-	<img src="/images/ODBVolunteer8.jpg"  alt="" />
+	<img src="/images/ODBVolunteer8.jpg" alt="" />
 </PageLayout>
+
+<style>
+	.donateButton {
+		padding: 4px 10px;
+	}
+</style>
